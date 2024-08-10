ab3f89e56b44e29f08f67108daddba62ac20fb34
BixGra <bixente.grandjean@gmail.com>
2024-08-11 01:59:57 +0200

    Initial commit

--- src/initialize.py
+++ src/initialize.py
@@ -26,8 +26,8 @@ def convert_google_sheet_url(url):
     return re.sub(pattern, lambda m: f'https://docs.google.com/spreadsheets/d/{m.group(1)}/export?' + (f'gid={m.group(3)}&' if m.group(3) else '') + 'format=csv', url)
 
 
-# url = convert_google_sheet_url("https://docs.google.com/spreadsheets/d/161ygk8Gyjn4JXMcP_fSjTJB776P0uZUsG7K8TTCYYV4/edit#gid=0")
-url = convert_google_sheet_url("https://docs.google.com/spreadsheets/d/161ygk8Gyjn4JXMcP_fSjTJB776P0uZUsG7K8TTCYYV4/edit#gid=885544863")
+url = convert_google_sheet_url("https://docs.google.com/spreadsheets/d/161ygk8Gyjn4JXMcP_fSjTJB776P0uZUsG7K8TTCYYV4/edit#gid=0")
+# url = convert_google_sheet_url("https://docs.google.com/spreadsheets/d/161ygk8Gyjn4JXMcP_fSjTJB776P0uZUsG7K8TTCYYV4/edit#gid=885544863")
 
 df = pd.read_csv(url)
 
