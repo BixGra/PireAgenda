77b4684c5a25bf305047ddd25b0f336dac9b5651
Bixente Grandjean <bixente.grandjean@gmail.com>
2025-10-21 22:03:00 +0200

    Subdomains

--- src/main.py
+++ src/main.py
@@ -79,4 +79,4 @@ def get_method():
 def get_method():
     logger.info("DB Refresh")
     with Refresher():
-        return RedirectResponse("https://pireagenda.sandbix.fr/")
+        return RedirectResponse("/")
